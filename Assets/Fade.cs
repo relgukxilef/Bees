ef9cede73fd868288f7786117efe7b25f92281bd
Felix Kugler <relgukxilef@gmail.com>
2024-06-17 01:25:16 +0200

    Draw background of inventory

--- Assets/Fade.cs
+++ Assets/Fade.cs
@@ -16,7 +16,8 @@ public class Fade : MonoBehaviour
     void Update()
     {
         spriteRenderer.color = new Color(
-            0, 0, 0, Mathf.Cos(World.day * Mathf.PI * 2) * 0.5f + 0.5f
+            0, 0, 0, 
+            Mathf.Pow(Mathf.Cos(World.day * Mathf.PI * 2) * 0.5f + 0.5f, 5.0f)
         );
     }
 }
